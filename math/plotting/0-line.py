9338cf66374e1d8b83797f14d52ef1b5382918ce
oviedofabrizzio1998 <oviedofabrizzio1998@gmail.com>
2025-08-15 14:07:41 +0000

    Task 0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+0-line.py
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+    """
+    Documented
+    """
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlim(0, 10)
+    plt.plot(range(0, 11), y, color='red', linestyle='-')
+    plt.show()
