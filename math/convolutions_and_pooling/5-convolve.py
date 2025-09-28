61b23687d188d840eee5c8cb2d23ff2c666dfd06
Fabrizzio <oviedofabrizzio1998@gmail.com>
2025-09-28 20:48:06 -0300

    task 5 and 6

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,1 +1,48 @@
 #!/usr/bin/env python3
+"""
+5-convolve.py
+"""
+import numpy as np
+
+
+def convolve(images, kernel, padding='same', stride=(1, 1)):
+    """
+    performs a convolution on images using myltiple kernels
+    """
+    kh, kw, kc, nc = kernels.shape
+    m, h, w, c = images.shape
+    sh, sw = stride
+    if kc != c:
+        raise ValueError("Kernel channels must match image channels.")
+
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
+        ph = pw = 0
+    else:
+        ph, pw = padding
+
+    padded_images = np.pad(
+        images,
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode='constant')
+
+    oh = (h + 2 * ph - kh) // sh + 1
+    ow = (w + 2 * pw - kw) // sw + 1
+    output = np.zeros((m, oh, ow, nc))
+
+    kernels = kernels.reshape((1, *kernels.shape))
+
+    for i in range(oh):
+        for j in range(ow):
+            h_start = i * sh
+            h_end = h_start + kh
+            w_start = j * sw
+            w_end = w_start + kw
+            output[:, i, j, :] = np.sum(
+                padded_images[:, h_start:h_end, w_start:w_end,
+                              :, None] * kernels,
+                axis=(1, 2, 3)
+            )
+    return output
--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+"""
+6-pool.py
+"""
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """
+    performs pooling on images
+    """
+    m, h, w, c = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    ph = pw = 0
+
+    oh = (h - kh + 2 * ph) // sh + 1
+    ow = (w - kw + 2 * pw) // sw + 1
+    output = np.zeros((m, oh, ow, c))
+
+    for i in range(oh):
+        for j in range(ow):
+            h_start = i * sh
+            h_end = h_start + kh
+            w_start = j * sw
+            w_end = w_start + kw
+            if mode == 'max':
+                output[:, i, j, :] = np.max(
+                    images[:, h_start:h_end, w_start:w_end, :],
+                    axis=(1, 2)
+                )
+            elif mode == 'avg':
+                output[:, i, j, :] = np.mean(
+                    images[:, h_start:h_end, w_start:w_end, :],
+                    axis=(1, 2)
+                )
+    return output
