4f27dcb78bfe08741c72b9e52ef71d7cc2da2c5d
Fabrizzio <oviedofabrizzio1998@gmail.com>
2025-12-03 15:37:40 -0300

    clustering task 2, 3 and 4

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+2-variance
+"""
+import numpy as np
+
+
+def variance(X, C):
+    """
+    calculates variance for a dataset
+    """
+    if not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray):
+        return None
+    if len(X.shape) != 2 or len(C.shape) != 2:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+    variance = np.sum(np.min(
+        np.linalg.norm(X[:, np.newaxis] - C, axis=2) ** 2, axis=1))
+    return variance
---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+3-optimum
+"""
+import numpy as np
+variance = __import__('2-variance').variance
+kmeans = __import__('1-kmeans').kmeans
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    tests for the optimum number of clusters by variance
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None
+    if kmax is not None and (not isinstance(kmax, int) or kmax <= 0):
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+    if kmax is None:
+        kmax = X.shape[0]
+    if kmin >= kmax:
+        return None, None
+    results = []
+    total_vars = []
+    d_vars = []
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append((C, clss))
+        current_var = variance(X, C)
+        total_vars.append(current_var)
+    for var in total_vars:
+        d_vars.append(np.sqrt((var - total_vars[0])**2))
+
+    return results, d_vars
