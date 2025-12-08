d28f358bcee343588f12e7c5d7a47cbd5e72b287
Fabrizzio <oviedofabrizzio1998@gmail.com>
2025-12-07 22:30:31 -0300

    task 7 to 12

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+7-maximization.py
+"""
+import numpy as np
+
+
+def maximization(X, g):
+    """
+    performs the maximization step in the EM algorithm for a GMM
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+    if (g.shape[1] != X.shape[0]):
+        return None, None, None
+    n, d = X.shape
+    if not np.isclose(np.sum(g, axis=0), np.ones(n,)).all():
+        return None, None, None
+    k = g.shape[0]
+    pi = np.sum(g, axis=1) / n
+    m = np.zeros((k, d))
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        g_i = g[i, :].reshape(-1, 1)
+        m[i] = np.sum(g_i * X, axis=0) / np.sum(g_i)
+        diff = X - m[i]
+        S[i] = (g_i * diff).T @ diff / np.sum(g_i)
+
+    return pi, m, S
