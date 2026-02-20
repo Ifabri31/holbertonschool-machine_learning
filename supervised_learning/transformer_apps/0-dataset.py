c718d2544bad55b313463f63d4446cd8c6291f34
Fabrizzio <oviedofabrizzio1998@gmail.com>
2026-02-19 21:52:19 -0300

    fixess

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -32,12 +32,17 @@ class Dataset:
         tokenizer_en = transformers.AutoTokenizer.from_pretrained(
             'bert-base-uncased', clean_up_tokenization_spaces=True)
 
-        pt_iter = (pt.decode('utf-8') for pt, _ in data.as_numpy_iterator())
-        en_iter = (en.decode('utf-8') for _, en in data.as_numpy_iterator())
+        def pt_iter():
+            for pt_batch, _ in data.batch(1000).as_numpy_iterator():
+                yield [s.decode('utf-8') for s in pt_batch]
+
+        def en_iter():
+            for _, en_batch in data.batch(1000).as_numpy_iterator():
+                yield [s.decode('utf-8') for s in en_batch]
 
         tokenizer_pt = tokenizer_pt.train_new_from_iterator(
-            pt_iter, 2**13)
+            pt_iter(), 2**13)
         tokenizer_en = tokenizer_en.train_new_from_iterator(
-            en_iter, 2**13)
+            en_iter(), 2**13)
 
         return tokenizer_pt, tokenizer_en
